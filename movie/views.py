40041b4281ba06746207f41b933cef3c3433dc30
jero98772 <jero98772@protonmail.com>
2024-02-07 10:06:07 -0500

    change name function

--- movie/views.py
+++ movie/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-	return render(request,"home.html",{"name":"greg lim"}) 
+	return render(request,"home.html",{"name":"Victor Daniel Arango Sohm"}) 
