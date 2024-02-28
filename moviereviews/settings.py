648472175a575dda6b24a61c0bd1476300117c74
jero98772 <jero98772@protonmail.com>
2024-02-28 10:29:33 -0500

    update new workshop

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movie',
+    'news',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'moviereviews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"moviereviews/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,9 +119,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICSFILES_DIR=[
+    os.path.join(BASE_DIR,"static"),
+    "moviereviews/static/",
+]
--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -15,16 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from movie import views as moviesViews
 
 from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', moviesViews.home),
+    path('', moviesViews.home,name="home"),
     path('about.html', moviesViews.about),
-
+    path('news/',include('news.urls')),
+    path('statistics/',moviesViews.statistics_view,name="statistics")
 ]
 
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
